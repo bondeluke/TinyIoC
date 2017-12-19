45a21c9a5a158c99ae1724b0eecf3e2da49d0f8b
Luke Bonde <bondeluke@gmail.com>
2017-12-18 23:59:19 -0600

    It's so Tiny

---
+++ TinyIoc/RegisterAndResolveTests.cs
@@ -0,0 +1,87 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinyIoc
+{
+    [TestClass]
+    public class RegisterAndResolveTests
+    {
+        [TestMethod]
+        public void Class()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register<Simple>();
+
+            Assert.IsNotNull(tiny.Resolve<Simple>());
+        }
+
+        [TestMethod]
+        public void Interface()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register<ISimple, Simple>();
+
+            Assert.IsNotNull(tiny.Resolve<ISimple>());
+        }
+
+        [TestMethod]
+        public void WithFactory()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register(factory => new Simple());
+
+            Assert.IsNotNull(tiny.Resolve<Simple>());
+        }
+
+        [TestMethod]
+        public void Delegate()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register<Simple>()
+                .Register<Func<Simple>>(factory => factory.Resolve<Simple>);
+
+            Assert.IsNotNull(tiny.Resolve<Func<Simple>>()());
+        }
+
+        [TestMethod]
+        public void ComplexClass()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register<Simple>()
+                .Register<ISimple, Simple>()
+                .Register<Func<Simple>>(factory => () => new Simple())
+                .Register<IComplex, Complex>();
+
+            var simpleInstance = tiny.Resolve<IComplex>();
+
+            Assert.IsNotNull(simpleInstance);
+        }
+
+        [TestMethod]
+        public void ThrowsOnReRegister()
+        {
+            var tiny = new TinyContainer();
+
+            tiny.Register<Simple>();
+
+            Assert.ThrowsException<TinyMistake>(() => tiny.Register<Simple>());
+            Assert.ThrowsException<TinyMistake>(() => tiny.Register(factory => new Simple()));
+        }
+
+        public interface ISimple { }
+
+        public class Simple : ISimple { }
+
+        public interface IComplex { }
+
+        public class Complex : IComplex
+        {
+            public Complex(Simple simple, ISimple iSimple, Func<Simple> complexClassFactory) { }
+        }
+    }
+}
---
+++ TinyIoc/TinyContainer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TinyIoc
+{
+    public interface ITinyRegistry
+    {
+        ITinyRegistry Register<T>();
+        ITinyRegistry Register<TService, TImplementation>() where TImplementation : TService;
+        ITinyRegistry Register<T>(Func<ITinyResolver, T> factory);
+    }
+
+    public interface ITinyResolver
+    {
+        T Resolve<T>();
+    }
+
+    public class TinyContainer : ITinyRegistry, ITinyResolver
+    {
+        private readonly IDictionary<Type, Type> _bindings;
+        private readonly IDictionary<Type, Func<ITinyResolver, object>> _instanceFactory;
+
+        public TinyContainer()
+        {
+            _bindings = new Dictionary<Type, Type>();
+            _instanceFactory = new Dictionary<Type, Func<ITinyResolver, object>>();
+        }
+
+        public ITinyRegistry Register<T>() => Register<T, T>();
+
+        public ITinyRegistry Register<TService, TImplementation>() where TImplementation : TService
+        {
+            var serviceKey = typeof(TService);
+
+            ThrowIfContains(serviceKey);
+
+            _bindings.Add(serviceKey, typeof(TImplementation));
+            return this;
+        }
+
+        public ITinyRegistry Register<T>(Func<ITinyResolver, T> factory)
+        {
+            var serviceKey = typeof(T);
+
+            ThrowIfContains(serviceKey);
+
+            _instanceFactory.Add(serviceKey, f => factory(f));
+
+            return this;
+        }
+
+        public T Resolve<T>() => (T)ResolveInternal(typeof(T));
+
+        private object ResolveInternal(Type t)
+        {
+            if (_bindings.ContainsKey(t))
+            {
+                var ctrs = _bindings[t].GetConstructors();
+
+                if (ctrs.Length == 0)
+                {
+                    return Activator.CreateInstance(t);
+                }
+
+                if (ctrs.Length > 1)
+                {
+                    throw new TinyMistake("Too many constructors.");
+                }
+
+                var instances = ctrs[0]
+                    .GetParameters()
+                    .Select(p => ResolveInternal(p.ParameterType))
+                    .ToArray();
+
+                return ctrs[0].Invoke(instances);
+            }
+
+            if (_instanceFactory.ContainsKey(t))
+            {
+                return _instanceFactory[t](this);
+            }
+
+            throw new TinyMistake($"Oh no. You didn't register {t.FullName}.");
+        }
+
+        private void ThrowIfContains(Type t)
+        {
+            if (_bindings.ContainsKey(t) || _instanceFactory.ContainsKey(t))
+            {
+                throw new TinyMistake($"You already registered {t.Name}.");   
+            }
+        }
+    }
+
+    public class TinyMistake : Exception
+    {
+        public TinyMistake(string message) : base(message) { }
+    }
+}
